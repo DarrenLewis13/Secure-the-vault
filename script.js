1abd65e0cf65d9feb374ae69e2f0ebd93192457f
Darren Lewis <darrenlewis@Darrens-MacBook-Pro.local>
2024-07-01 08:24:52 -0400

    Secure the vault commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// 3 variables that each add up to 10,40, and 39.
+const first = 7 + 3;
+const second = 120/3;
+const third = (second - 1)
+// Display the combination in an alert pop-up
+const combination = first + '-' + second + '-' + third;
+const userString= "you have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+alert(userString + ' ' + combination);
